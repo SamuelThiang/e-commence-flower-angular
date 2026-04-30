3484ea96acfbcc0c24402d6d1d3e471bcf002b05
SamuelThiang <69668168+SamuelThiang@users.noreply.github.com>
2026-05-01 06:25:27 +0800

    Update google-gis.d.ts

--- src/google-gis.d.ts
+++ src/google-gis.d.ts
@@ -9,11 +9,18 @@ declare global {
           initialize(config: {
             client_id: string;
             callback: (response: { credential: string }) => void;
+            auto_select?: boolean;
+            ux_mode?: 'popup' | 'redirect';
+            login_uri?: string;
+            cancel_on_tap_outside?: boolean;
+            itp_support?: boolean;
+            use_fedcm_for_prompt?: boolean;
           }): void;
           renderButton(
             parent: HTMLElement,
             options: Record<string, string | number | boolean>,
           ): void;
+          prompt(): void;
         };
       };
     };
